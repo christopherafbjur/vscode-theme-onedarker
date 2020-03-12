6d4278bc3842807dbe7b0a162c777abccc6eee22
Christopher Af Bjur <chrisdevcabc@gmail.com>
2020-03-12 16:32:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Atom One Darker
+
+This is a vscode theme inspired by the [Atom One Dark UI theme](https://atom.io/themes/one-dark-ui) but with a higher contrast.
+
+I will add more languages frequently. Currently only vanilla javascript is supported fully.
+
+If you find any bugs or have any requests please file an issue.
---
+++ samples/javascript.js
@@ -0,0 +1,214 @@
+const json = {
+	"str": "top",
+	"num": 1337,
+	"bool": true,
+}
+
+const s = {
+  methodName: function (attribute) {
+    
+  },
+  key: 'value'
+}
+
+ClassName.prototype.methodName = function () {
+	
+};
+
+do {
+	
+} while (true);
+
+condition ? true : false
+
+
+if (true) {
+	
+}
+
+
+if (true) {
+	
+} else if(false) {
+
+} else {
+	
+}
+
+
+function(){
+  for (var i = 0; i < array.length; i++) {
+    array[i];
+  };
+}
+
+
+for (var variable in object) {
+	if (object.hasOwnProperty(variable)) {
+		
+	}
+}
+
+
+for (variable of iterable) {
+	
+}
+
+
+function functionName() {
+	
+}
+
+
+function () {
+	
+}
+
+
+() => {
+	
+}
+
+
+function* functionName() {
+	
+}
+
+
+function* () {
+	
+}
+
+getElementsByClassName('className')
+
+(function() {
+	'use strict';
+}());
+
+console.log();
+console.dir();
+console.warn();
+console.error();
+
+
+
+console.log(require('test').inspect( { depth: null }));
+
+
+
+new Promise(function(resolve, reject) {
+	
+});
+
+
+
+setInterval(function () {
+	
+}, 10);
+
+
+
+setTimeout(function () {
+	
+}, 10);
+
+
+
+switch (expression) {
+	case expression:
+		
+		break;
+	default:
+		
+}
+
+const test;
+
+if(test == 2){
+
+}else if(true === false){
+
+}else if ('str' === typeof 'string'){
+  
+}else if(1 === 2){
+
+}else{
+
+}
+
+try {
+	
+} catch (e) {
+	
+} finally {
+	
+}
+
+
+
+while (true) {
+	
+}
+
+
+//Comment
+
+/**
+ * Block Comment
+ */
+
+
+
+
+const module = require('module');
+
+
+
+class ClassName {
+	constructor(asd) {
+		
+	}
+}
+
+
+
+exports.functionName = function () {
+
+};
+
+
+
+module.exports = name;
+
+
+
+return ;
+
+
+const arr = [1, 2, 3];
+const obj = {taco: 1, talo: 'haj', teno: true}
+const ladi = `${obj.taco} sheep jumpin down the lane.`;
+const testa = [...arr];
+const testo = {...obj};
+const {taco, talo} = obj;
+
+export default {
+	title: 'taco'
+}
+
+import Tamo from 'ladiko';
+Tamo.cata;
+
+export default {
+  title: "String",
+  fieldsets: [
+    {
+      title: "String",
+      options: { collapsible: true, num: 100 }
+    }
+  ],
+  fields: [
+    test.Test,
+    ...Testing,
+  ]
+};
